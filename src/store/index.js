ce007b5e713f2d1a10f946ea546b8d776e44fe16
Josué Camelo <cloudone@josuecamelo.com>
2023-01-07 10:25:19 -0300

    Adicionado Item ao Carrinho

--- src/store/index.js
+++ src/store/index.js
@@ -3,19 +3,27 @@ import axios from 'axios'
 
 export default createStore({
   state: {
-    products: [1,2,3]
+    products: [],
+    productsInBag: []
   },
   mutations: {
     loadProducts(state, products){
       state.products = products;
+    },
+    addToBagMutation(state, product){
+      console.log('adicionado');
+      state.productsInBag.push(product);
     }
   },
   actions: {
     loadProducts({ commit }){
       axios.get('https://fakestoreapi.com/products')
       .then((response) => {
-        commit('loadProducts', response.data)
+        commit('loadProducts', response.data);
       })
+    },
+    addToBagAction({commit}, product){
+      commit('addToBagMutation', product);
     }
   },
   modules: {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,10 +8,11 @@
         <div class="product-image" :style="{backgroundImage: 'url('+ product.image + ')'}"></div>
         <h4>{{ product.title }}</h4>
         <p class="price">R$ {{ product.price.toFixed(2) }}</p>
-        <button>Adicionar ao carrinho</button>
+        <button @click="addToBag(product)">Adicionar ao carrinho</button>
       </div>
 
     </div>
+    {{ productsInBag.length }}
   </div>
 </template>
 
@@ -27,10 +28,16 @@ export default {
   computed:{
     products(){
       return this.$store.state.products
+    },
+    productsInBag(){
+      return this.$store.state.productsInBag;
     }
   },
   methods: {
-   
+   addToBag(product){
+      product.quantity = 1;
+      this.$store.dispatch('addToBagAction', product);
+   }
   }
 }
 </script>
